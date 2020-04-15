9d58c07c8cf77892237c603185f13f9e3352a850
Jukka Aho <ahojukka5@gmail.com>
2020-04-15 23:56:50 +0700

    Refactor code
    Now have two types, QuickFind and QuickUnion, and QuickUnion has extra
    options to turn off weighting and/or path compression, if needes for
    some reason.

--- src/Judycon.jl
+++ src/Judycon.jl
@@ -1,9 +1,98 @@
 module Judycon
 
-import Base: union
+"""
+    QuickFind
+"""
+struct QuickFind
+    id :: Vector{Int}
+end
 
-include("quickfind.jl")
-include("quickunion.jl")
+function QuickFind(n)
+    id = collect(1:n)
+    return QuickFind(id)
+end
+
+function find(qf::QuickFind, p::Int)
+    id = qf.id
+    return id[p]
+end
+
+function isconnected(qf::QuickFind, p::Int, q::Int)
+    i = find(qf, p)
+    j = find(qf, q)
+    return isequal(i, j)
+end
+
+function connect!(qf::QuickFind, p::Int, q::Int)
+    id = qf.id
+    pid = id[p]
+    qid = id[q]
+    n = length(id)
+    for i=1:n
+        if isequal(getindex(id, i), pid)
+            setindex!(id, qid, i)
+        end
+    end
+    return
+end
+
+"""
+    QuickUnion
+"""
+struct QuickUnion
+    id :: Vector{Int}
+    sz :: Vector{Int}
+    weighted :: Bool
+    compress :: Bool
+end
+
+function QuickUnion(n::Int, weighted=true, compress=true)
+    id = collect(1:n)
+    sz = zeros(Int, n)
+    return QuickUnion(id, sz, weighted, compress)
+end
+
+function find(qu::QuickUnion, p::Int)
+    id = qu.id
+    while p != id[p]
+        if qu.compress
+            id[p] = id[id[p]]
+        end
+        p = id[p]
+    end
+    return p
+end
+
+function isconnected(qu::QuickUnion, p::Int, q::Int)
+    i = find(qu, p)
+    j = find(qu, q)
+    return isequal(i, j)
+end
+
+"""
+    connect(G, p, q)
+
+Connect p and q in G.
+
+If weighting is used, connect such a way that the depth of the tree is minimized.
+"""
+function connect!(qu::QuickUnion, p::Int, q::Int)
+    id = qu.id
+    i = find(qu, p)
+    j = find(qu, q)
+    if qu.weighted
+        sz = qu.sz
+        if (sz[i] < sz[j])
+            id[i] = j
+            sz[j] += sz[i]
+        else
+            id[j] = i
+            sz[i] += sz[j]
+        end
+    else
+        id[i] = j
+    end
+end
 
 export QuickFind, QuickUnion, connect!, isconnected, find
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,49 @@
-using Test
+using Judycon, Test
+
+#
+# 1   2---3   4---5
+# |   |   |   |   |
+# 6---7   8   9   10
+#
+
+G1 = QuickFind(10);
+G2 = QuickUnion(10, false, false);
+G3 = QuickUnion(10, true, false);
+G4 = QuickUnion(10, false, true);
+G5 = QuickUnion(10, true, true);
+
+function test(G)
+
+    connect!(G, 1, 6)
+    connect!(G, 6, 7)
+    connect!(G, 7, 2)
+    connect!(G, 2, 3)
+    connect!(G, 3, 8)
+    
+    connect!(G, 9, 4)
+    connect!(G, 4, 5)
+    connect!(G, 5, 10)
+
+    # `pts1` should form one set of connected components and `pts2` another, respectively.
+    pts1 = [1, 2, 3, 6, 7, 8]
+    pts2 = [4, 5, 9, 10]
+    for p in pts1
+        for q in pts1
+            @test isconnected(G, p, q)
+            @test isconnected(G, q, p)
+        end
+        for q in pts2
+            @test !isconnected(G, p, q)
+            @test !isconnected(G, q, p)
+        end
+    end
+end
 
 @testset "Test Judycon.jl" begin
-    @testset "Test QuickFind" begin include("test_qf.jl") end
-    @testset "Test QuickUnion" begin include("test_qu.jl") end
+    @testset "Test QuickFind" begin test(G1) end
+    @testset "Test QuickUnion without weighting and path compression" begin test(G2) end
+    @testset "Test QuickUnion with weighting and without path compression" begin test(G3) end
+    @testset "Test QuickUnion without weighting and with path compression" begin test(G4) end
+    @testset "Test QuickUnion with weighting and path compression" begin test(G5) end
 end
+
