52c484323fdd7ec67e5fa86dece5bc7af6994f9d
Jukka Aho <ahojukka5@gmail.com>
2020-04-20 09:02:15 +0700

    Update percolation model

--- demos/percolation.jl
+++ demos/percolation.jl
@@ -6,21 +6,20 @@ import Judycon: connect!
 struct Percolation
     grid :: Matrix{Bool}
     li :: LinearIndices
-    dim :: Int
+    ny :: Int
+    nx :: Int
     wuf1 :: QuickUnion
     wuf2 :: QuickUnion
-    topnode :: Int
-    bottomnode :: Int
+    node1 :: Int
+    node2 :: Int
 end
 
-function Percolation(dim)
-    grid = zeros(Bool, dim, dim)
+function Percolation(ny, nx)
+    grid = zeros(Bool, ny, nx)
     li = LinearIndices(grid)
-    wuf1 = QuickUnion(dim^2+2)
-    wuf2 = QuickUnion(dim^2+1)
-    topnode = dim^2 + 1
-    bottomnode = topnode + 1
-    return Percolation(grid, li, dim, wuf1, wuf2, topnode, bottomnode)
+    wuf1 = QuickUnion(nx*ny + 2)
+    wuf2 = QuickUnion(nx*ny + 1)
+    return Percolation(grid, li, ny, nx, wuf1, wuf2, nx*ny + 1, nx*ny + 2)
 end
 
 function connect!(p::Percolation, i, j)
@@ -29,7 +28,7 @@ function connect!(p::Percolation, i, j)
 end
 
 function isopen(p::Percolation, i, j)
-    return p.grid[i,j]
+    return p.grid[i, j]
 end
 
 function number_of_open(p::Percolation)
@@ -38,22 +37,24 @@ end
 
 function isfull(p::Percolation, i, j)
     !isopen(p, i, j) && return false
-    return isconnected(p.wuf2, p.topnode, p.li[i,j])
+    return isconnected(p.wuf2, p.node1, p.li[i, j])
 end
 
 function percolates(p::Percolation)
-    return isconnected(p.wuf1, p.topnode, p.bottomnode)
+    return isconnected(p.wuf1, p.node1, p.node2)
 end
 
 function open!(p::Percolation, i, j)
     isopen(p, i, j) && return
     p.grid[i, j] = true
-    i == 1      && connect!(p, p.topnode, p.li[i, j])
-    i == p.dim  && connect!(p.wuf1, p.bottomnode, p.li[i, j])
-    i > 1       && isopen(p, i-1, j) && connect!(p, p.li[i,j], p.li[i-1, j])
-    i < p.dim-1 && isopen(p, i+1, j) && connect!(p, p.li[i,j], p.li[i+1, j])
-    j > 1       && isopen(p, i, j-1) && connect!(p, p.li[i,j], p.li[i, j-1])
-    j < p.dim-1 && isopen(p, i, j+1) && connect!(p, p.li[i,j], p.li[i, j+1])
+
+    i == 1       && connect!(p, p.node1, p.li[i, j])
+    i == p.ny    && connect!(p.wuf1, p.node2, p.li[i, j])
+
+    i > 1        && isopen(p, i-1, j) && connect!(p, p.li[i, j], p.li[i-1, j])
+    i < p.ny - 1 && isopen(p, i+1, j) && connect!(p, p.li[i, j], p.li[i+1, j])
+    j > 1        && isopen(p, i, j-1) && connect!(p, p.li[i, j], p.li[i, j-1])
+    j < p.nx - 1 && isopen(p, i, j+1) && connect!(p, p.li[i, j], p.li[i, j+1])
 end
 
 using Statistics
@@ -86,10 +87,10 @@ function Base.show(io::IO, r::Result)
     @printf("% 25s = [%0.3f, %0.3f]\n", "95% confidence interval", r.confidence_lo, r.confidence_hi)
 end
 
-function run_one(n)
-    p = Percolation(n)
+function run_one(ny, nx)
+    p = Percolation(ny, nx)
     while !percolates(p)
-        open!(p, rand(1:n), rand(1:n))
+        open!(p, rand(1:ny), rand(1:nx))
     end
     return p
 end
@@ -97,7 +98,7 @@ end
 function run(n, trials)
     thresholds = zeros(trials)
     for i=1:trials
-        p = run_one(n)
+        p = run_one(n, n)
         thresholds[i] = number_of_open(p) / n^2
     end
     return Result(thresholds)
